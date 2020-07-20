5bf37fba326ee4354fe7320c5c54f1eb8cfbc4c2
Lucas Santana <lucas_377@hotmail.com>
2020-07-20 20:40:20 -0300

    Ajustando logo no menu fluído

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ jQuery(function () {
                 $("nav#menu ul li").css('line-height', '60px');
                 $("nav#menu ul li a").css('font-size', '11px');
                 $("nav#menu").css('height', '60px');
+                $("#logo").css('width', '40px');
+                $("#logo").css('height', '40px');
+                $("#logo").css('margin-top', '9px');
             }
         }
 
@@ -14,6 +17,9 @@ jQuery(function () {
             $("nav#menu").css('height', '80px');
             $("nav#menu ul li").css('line-height', '80px');
             $("nav#menu ul li a").css('font-size', '13px');
+            $("#logo").css('width', '50px');
+            $("#logo").css('height', '50px');
+            $("#logo").css('margin-top', '15px');
         }
     });
 });
