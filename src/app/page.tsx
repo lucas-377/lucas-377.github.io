f0c6b4a800f7bae3c992fb154122098bafb76687
Lucas Santana <lucas_377@hotmail.com>
2025-06-26 20:36:21 -0300

    feat: Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Header from "@/components/header";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>Heelo World</h1>
+    <>
+      <Header />
+      <main>
+        <div className="container mx-auto">
+          <h1>Heelo World</h1>
+        </div>
       </main>
-    </div>
+    </>
   );
 }
